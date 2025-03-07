69e24f0f8c452f525511932613fc250451aa59e7
Xian Saiful <xiansaiful@gmail.com>
2025-03-08 03:04:16 +0600

    Added localhost

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const isDevelopment = window.location.hostname === "localhost"; // Check if running on localhost
+
 export const axiosInstance = axios.create({
-    baseURL: "https://kitty-chat-backend-psi.vercel.app/api/",
+    baseURL: isDevelopment
+        ? "http://localhost:5002/api/" // Local backend URL
+        : "https://kitty-chat-backend-psi.vercel.app/api/", // Live backend URL
     withCredentials: true
 });
